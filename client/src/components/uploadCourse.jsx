d33e9f1f4d9591d5c9f8b8aa594619942a085964
Shree2124 <shreealasande@gmail.com>
2024-09-28 23:55:19 +0530

    changes

--- client/src/components/uploadCourse.jsx
+++ client/src/components/uploadCourse.jsx
@@ -10,12 +10,15 @@ import {
   Select,
   MenuItem,
 } from '@mui/material';
+import axiosInstance from "../utils/axios.js"
 
 const CreateCourse = () => {
   const [courseTitle, setCourseTitle] = useState('');
   const [courseDescription, setCourseDescription] = useState('');
   const [courseCategory, setCourseCategory] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
+  const [courseDuration, setCourseDuration] = useState(null);
+  const {user} = 
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +27,16 @@ const CreateCourse = () => {
       courseDescription,
       courseCategory,
       thumbnail,
+      
     });
+
+    axiosInstance.post("/course/new",{
+      title: courseTitle,
+      description:courseDescription,
+      category: courseCategory,
+      duration: courseDuration,
+      createdBy: 
+    })
   };
 
   const handleThumbnailChange = (e) => {
@@ -76,8 +88,18 @@ const CreateCourse = () => {
                 <MenuItem value="Business">Business</MenuItem>
                 <MenuItem value="Photography">Photography</MenuItem>
                 <MenuItem value="Music">Music</MenuItem>
-                {/* Add more categories as needed */}
               </Select>
+            </Box>
+            <Box item xs={12}>
+              <TextField
+                type='number'
+                label="Duration"
+                variant="outlined"
+                fullWidth
+                value={courseDuration}
+                onChange={(e) => setCourseDuration(e.target.value)}
+                required
+              />
             </Box>
             <Box item xs={12}>
               <input
