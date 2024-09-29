d3050305e33e7c2ba8add9758d4c83cb18f0d7c9
Shree2124 <shreealasande@gmail.com>
2024-09-29 19:20:03 +0530

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -6,21 +6,23 @@ const app = express();
 
 app.use(
   cors({
-    origin:["https://scroll-hack-4glf.vercel.app"],
-    credentials: true,
+    origin: ["https://scroll-hack-4glf.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    preflightContinue: false,
+    credentials: true
   })
 );
 
-  app.use(
-    express.json({
-      limit: "16kb",
-    })
-  );
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("hello")
 })
 
@@ -28,8 +30,8 @@ import userRouter from "./routes/user.routes.js"
 import adminRouter from "./routes/admin.routes.js"
 import lectureRoutes from "./routes/course.routes.js"
 
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1",adminRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1", adminRouter)
 app.use("/api/v1", lectureRoutes)
 
 
